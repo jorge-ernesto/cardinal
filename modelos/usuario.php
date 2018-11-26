82b034ec3243c6d8acd2d3f09f2bb61e221d7f65
Jorge <jorge.cywdt@gmail.com>
2018-11-26 00:40:50 -0500

    045 Gestión de Usuarios Modelo

---
+++ modelos/usuario.php
@@ -0,0 +1,53 @@
+<?php
+
+require '../conexion/conexion.php';
+
+class usuario {
+
+    public function __construct() {
+    }
+
+    public function listar() {
+        $sql = "select   *
+                from     usuarios
+                order by id";
+        return execute($sql);
+    }
+
+    public function buscar($id) {
+        $sql = "select *
+                from   usuarios
+                where  id = '$id'";
+        return findById($sql);
+    }
+
+    public function guardar($nombre, $tipoDocumento, $numDocumento, $direccion, $telefono, $email, $cargo, $login, $clave, $imagen) {
+        $sql = "insert into usuarios (nombre,tipo_documento,num_documento,direccion,telefono,email,cargo,login,clave,imagen,estado)
+                values('$nombre','$tipoDocumento','$numDocumento','$direccion','$telefono','$email','$cargo','$login','$clave','$imagen','1')";
+        return execute($sql);
+    }
+
+    public function editar($id, $nombre, $tipoDocumento, $numDocumento, $direccion, $telefono, $email, $cargo, $login, $clave, $imagen, $estado) {
+        $sql = "update usuarios
+                set    nombre = '$nombre',tipo_documento = '$tipoDocumento',num_documento = '$numDocumento',direccion = '$direccion',telefono = '$telefono',email = '$email',cargo = '$cargo',login = '$login',clave = '$clave',imagen = '$imagen',estado = '$estado'
+                where  id = '$id'";
+        return execute($sql);
+    }
+
+    public function desactivar($id) {
+        $sql = "update usuarios
+                set    estado = '0'
+                where  id = '$id'";
+        return execute($sql);
+    }
+
+    public function activar($id) {
+        $sql = "update usuarios
+                set    estado = '1'
+                where  id = '$id'";
+        return execute($sql);
+    }
+
+}
+
+?>
