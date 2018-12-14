db749081e4214b44faa7124edfd15c3b72677d40
Jorge <jorge.cywdt@gmail.com>
2018-12-14 05:38:00 -0500

    KKK

--- vistas/js/login.js
+++ vistas/js/login.js
@@ -9,8 +9,25 @@ $('#formulario').on('submit', function(e) {
         if (data == null) {
             swal('Login', 'Error en el login: Nombre de usuario o contraseña incorrecta, por favor vuelva a intentarlo', 'error')
         } else {
+            // swal('Login', 'Hola ' + data.nombre + ' haz iniciado sesión con éxito', 'success')
             // $(location).attr('href', 'listarCategoria.php')
-            swal('Login', 'Hola ' + data.nombre + ' haz iniciado sesión con éxito', 'success')
+
+            swal({
+                title: 'Hola ' + data.nombre + ' haz iniciado sesión con éxito',
+                width: 600,
+                padding: '3em',
+                background: '#fff url(vendors/img/trees.png)',
+                backdrop: `
+                rgba(0,0,123,0.4)
+                url("vendors/img/nyan-cat.gif")
+                center left
+                no-repeat
+                `
+            }).then((result) => {
+                if (result.value) {
+                    $(location).attr('href', 'listarCategoria.php')
+                }
+            })
         }
     });
 });
