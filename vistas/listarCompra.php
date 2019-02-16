8633214658d54fe379db2c9050e880bcd35a13c2
Jorge <jorge.cywdt@gmail.com>
2019-02-15 20:44:20 -0500

    aaa

--- vistas/listarCompra.php
+++ vistas/listarCompra.php
@@ -193,7 +193,7 @@ if (!isset($_SESSION['nombre'])) { // Si la variable es null
                 </div>
 
                 <!-- Modal -->
-                <div class="modal fade bd-example-modal-lg" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div id="exampleModal" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div class="modal-dialog modal-lg" role="document">
                         <div class="modal-content">
                             <div class="modal-header">
